ca85d46ba2cbac0421c1ceb157739dbb60dc66c4
Ryder <ryder3222@gmail.com>
2024-05-15 11:10:19 -0500

    camera ui rotation

--- dist/assets/md.transition-legacy-CErfiBso.js
+++ dist/assets/md.transition-legacy-CErfiBso.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BLuPShx3.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.e,i=e.d}],execute:function(){
+System.register(["./index-legacy-Da5tZnnf.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.e,i=e.d}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
