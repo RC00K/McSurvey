88c59f701300b101731a0a6741259607dd177d13
Ryder <ryder3222@gmail.com>
2024-05-16 15:27:35 -0500

    camera styles update

--- dist/assets/md.transition-legacy-b2TvHVLf.js
+++ dist/assets/md.transition-legacy-b2TvHVLf.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BrPLUPAZ.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.e,i=e.d}],execute:function(){
+System.register(["./index-legacy-BbmZpgYG.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.e,i=e.d}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
